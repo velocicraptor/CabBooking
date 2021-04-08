607dc044031985a493863c91705b1d51bdbcb2f0
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-09 04:32:36 +0530

    Update TripBooking.java

--- src/main/java/com/cg/entities/TripBooking.java
+++ src/main/java/com/cg/entities/TripBooking.java
@@ -16,13 +16,13 @@ import java.time.LocalDateTime;
 public class TripBooking {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="trip_booking_id",unique = true, nullable = false)
+	@Column(name = "trip_booking_id", unique = true, nullable = false)
 	private int tripBookingId;
 	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name="customer_id",unique=true)
+	@JoinColumn(name = "customer_id", unique = true)
 	private Customer customer;
 	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name="driver_id")
+	@JoinColumn(name = "driver_id")
 	private Driver driver;
 	private String fromLocation;
 	private String toLocation;
@@ -31,11 +31,11 @@ public class TripBooking {
 	private boolean status;
 	private float distanceInKm;
 	private float bill;
-	
+
 	public TripBooking() {
 
 	}
-	
+
 	public TripBooking(Customer customer, Driver driver, String fromLocation, String toLocation,
 			LocalDateTime fromDateTime, LocalDateTime toDateTime, boolean status, float distanceInKm, float bill) {
 		super();
@@ -50,92 +50,92 @@ public class TripBooking {
 		this.bill = bill;
 	}
 
-
-
-
-
 	public int getTripBookingId() {
 		return tripBookingId;
 	}
+
 	public void setTripBookingId(int tripBookingId) {
 		this.tripBookingId = tripBookingId;
 	}
-	
+
 	public Driver getDriver() {
 		return driver;
 	}
+
 	public void setDriver(Driver driver) {
 		this.driver = driver;
 	}
+
 	public String getFromLocation() {
 		return fromLocation;
 	}
+
 	public void setFromLocation(String fromLocation) {
 		this.fromLocation = fromLocation;
 	}
+
 	public String getToLocation() {
 		return toLocation;
 	}
+
 	public void setToLocation(String toLocation) {
 		this.toLocation = toLocation;
 	}
+
 	public LocalDateTime getFromDateTime() {
 		return fromDateTime;
 	}
+
 	public void setFromDateTime(LocalDateTime fromDateTime) {
 		this.fromDateTime = fromDateTime;
 	}
+
 	public LocalDateTime getToDateTime() {
 		return toDateTime;
 	}
+
 	public void setToDateTime(LocalDateTime toDateTime) {
 		this.toDateTime = toDateTime;
 	}
+
 	public boolean isStatus() {
 		return status;
 	}
+
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
+
 	public float getDistanceInKm() {
 		return distanceInKm;
 	}
+
 	public void setDistanceInKm(float distanceInKm) {
 		this.distanceInKm = distanceInKm;
 	}
+
 	public float getBill() {
 		return bill;
 	}
+
 	public void setBill(float bill) {
 		this.bill = bill;
 	}
-
-
-
-
 
 	public Customer getCustomer() {
 		return customer;
 	}
 
-
-
-
-
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
 
-@Override
+	@Override
 	public String toString() {
 		return "TripBooking [tripBookingId=" + tripBookingId + ", customer=" + customer + ", driver=" + driver
 				+ ", fromLocation=" + fromLocation + ", toLocation=" + toLocation + ", fromDateTime=" + fromDateTime
 				+ ", toDateTime=" + toDateTime + ", status=" + status + ", distanceInKm=" + distanceInKm + ", bill="
 				+ bill + "]";
 	}
-	
-	
-	
-	
-	
+
 }
