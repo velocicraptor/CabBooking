72fb712dc4fa9092378b1113ab19329d86fbb809
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-09 04:35:16 +0530

    Update TripBooking.java

--- src/main/java/com/cg/entities/TripBooking.java
+++ src/main/java/com/cg/entities/TripBooking.java
@@ -9,7 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-
 import java.time.LocalDateTime;
 
 @Entity
