dd03fb63cf14482f22d8e2a937e12499f4f5b05c
S-sanjana592 <80334944+S-sanjana592@users.noreply.github.com>
2021-04-09 05:02:45 +0530

    Update TripBookingAlreadyExistsException.java

--- src/main/java/com/cg/exception/TripBookingAlreadyExistsException.java
+++ src/main/java/com/cg/exception/TripBookingAlreadyExistsException.java
@@ -1,5 +1,11 @@
 package com.cg.exception;
 
-public class TripBookingAlreadyExistsException {
+public class TripBookingAlreadyExistsException  extends Exception {
+
+	private static final long serialVersionUID = 914569459626622748L;
+
+	public TripBookingAlreadyExistsException(String msg) {
+		super(msg);
+	} 
 
 }
